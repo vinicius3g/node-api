401ee92986ce10ff18731e5ef771c36aa0de3a24
vinicius dos santos paixao <vinicius.ds.paixao@gmail.com>
2020-04-08 00:12:07 -0300

    iniciação do banco de dados com mongoose

--- api.js
+++ api.js
@@ -1,9 +1,19 @@
+const mongoose = require('mongoose');
 const express = require('express');
+const requireDir = require('require-dir');
 
+//iniciando o app
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.send('servidor iniciado!!')
-})
+//iniciando db
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}); 
 
-app.listen(3001);
+requireDir('./src/models');
+
+//rotas
+app.use('/api', require('./src/routes'));
+
+app.listen(3005);
