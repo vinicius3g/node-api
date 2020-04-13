73a10d2beb4ae9aee844050c54eabc7b05994681
vinicius dos santos paixao <vinicius.ds.paixao@gmail.com>
2020-04-12 23:24:51 -0300

    adicionando cors e envio de dados no formato json

--- api.js
+++ api.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 //iniciando o app
 const app = express();
+//permitir envio de dados no forato de json 
+app.use(express.json());
+//permitir que outro endereços acessem a aplicação ("basico da lib")
+app.use(cors());
 
 //iniciando db
 mongoose.connect('mongodb://localhost:27017/nodeapi', {
