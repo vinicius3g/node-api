d104a489844c912fade25fa90f1fdf9f41b81887
vinicius dos santos paixao <vinicius.ds.paixao@gmail.com>
2020-04-12 23:25:38 -0300

    rotas criadas

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,10 @@ const routes = express.Router();
 const ProductController = require('./controllers/ProductController');
 
 //primeira rota
-routes.get('/products', ProductController.index)
+routes.get('/products', ProductController.index);
+routes.post('/products',ProductController.store);
+routes.get('/products/:id', ProductController.show);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.destroy);
 
 module.exports = routes;
